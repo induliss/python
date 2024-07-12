be1b85931ee0f9ea37597242e701cff1cd8e7223
Indulis Straume <130855875+induliss@users.noreply.github.com>
2024-07-12 15:24:13 +0300

    Update import webbrowser.py

--- import webbrowser.py
+++ import webbrowser.py
@@ -3,8 +3,8 @@ import json
 from urllib.request import urlopen
 
 print("Let's find an old website.")
-site = input("Type a website URL: ")
-era = input("Type a year, month, and day, like 20150613: ")
+site = input("Type a website URL: lolcats.com")
+era = input("Type a year, month, and day, like 20150613: 2015, 11, 02")
 url = "http://archive.org/wayback/available?url=%s&timestamp=%s" % (site, era)
 response = urlopen(url)
 contents = response.read()
